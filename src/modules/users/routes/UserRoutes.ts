7d17cb767f64c3e6cef8dcb67f30effd050afdb6
Alvaro6800 <alvarolgsouza2009@gmail.com>
2025-08-05 17:13:44 -0300

    feat: user and products schemas with celebrate

--- src/modules/users/routes/UserRoutes.ts
+++ src/modules/users/routes/UserRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import UsersControllers from "../controllers/UsersControllers";
+import { createUserSchema, idParamsValidation } from "../schemas/UserSchemas";
+import { updateProductSchema } from "@modules/products/schemas/ProductSchemas";
 
 const usersRouter = Router();
 const usersController = new UsersControllers();
 
 usersRouter.get("/", usersController.index);
-usersRouter.get("/:id", usersController.show);
-usersRouter.post("/", usersController.create);
-usersRouter.put("/:id", usersController.update);
-usersRouter.delete("/:id", usersController.delete);
+usersRouter.get("/:id", idParamsValidation, usersController.show);
+usersRouter.post("/", createUserSchema, usersController.create);
+usersRouter.put("/:id", updateProductSchema, usersController.update);
+usersRouter.delete("/:id", idParamsValidation, usersController.delete);
 
 export default usersRouter;
---
+++ src/modules/users/schemas/UserSchemas.ts
@@ -0,0 +1,28 @@
+import { celebrate, Joi, Segments } from "celebrate";
+
+export const createUserSchema = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    avatar: Joi.string().uri().optional(),
+  }),
+});
+
+export const updateUserSchema = celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required(),
+  }),
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    avatar: Joi.string().uri().optional(),
+  }),
+});
+
+export const idParamsValidation = celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required(),
+  }),
+});
