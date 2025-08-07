046b4242f758f8791938938732fca891a3eabd48
Alvaro6800 <alvarolgsouza2009@gmail.com>
2025-08-06 23:52:20 -0300

    feat: user token migration, entity and sendForgotPasswordEmailService

---
+++ src/modules/users/database/entities/UserTokens.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("user_tokens")
+export class UserTokens {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @Generated("uuid")
+  token: string;
+
+  @Column()
+  user_id: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,8 @@ import productsRouter from "@modules/products/routes/ProductRoutes";
 import avatarRouter from "@modules/users/routes/AvatarRoutes";
 import sessionsRouter from "@modules/users/routes/SessionRoutes";
 import usersRouter from "@modules/users/routes/UserRoutes";
-import { Router } from "express";
+import express, { Router } from "express";
+import uploadConfig from "@config/upload";
 
 const routes = Router();
 
@@ -13,6 +14,7 @@ routes.get("/health", (req, res) => {
 routes.use("/products", productsRouter);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
-routes.use("/avatar", avatarRouter)
+routes.use("/avatar", avatarRouter);
+routes.use("/files", express.static(uploadConfig.directory));
 
 export default routes;
