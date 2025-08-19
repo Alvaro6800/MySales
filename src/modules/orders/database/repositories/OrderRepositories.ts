707fa4e3acf2532cb06dc8b07a43cafac630e728
Alvaro6800 <alvarolgsouza2009@gmail.com>
2025-08-18 21:51:23 -0300

    feat: order repositories

---
+++ src/modules/orders/database/repositories/OrderRepositories.ts
@@ -0,0 +1,31 @@
+import { AppDataSource } from "@shared/typeorm/data-source";
+import { Order } from "../entities/Orders";
+import { Customer } from "@modules/customers/database/entities/Customer";
+import { OrdersProducts } from "../entities/OrdersProducts";
+
+interface ICreateOrder {
+  customer: Customer;
+  products: OrdersProducts[];
+}
+
+export const orderRepositories = AppDataSource.getRepository(Order).extend({
+  async findById(id: number): Promise<Order | null> {
+    const order = this.findOne({
+      where: { id },
+      relations: ["order_products", "costumer"],
+    });
+
+    return order;
+  },
+
+  async createOrder({ customer, products }: ICreateOrder): Promise<Order> {
+    const order = this.create({
+      customer,
+      order_products: products,
+    });
+
+    await this.save(order);
+
+    return order;
+  },
+});
